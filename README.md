7c249c29cecfbf187a1c9ba19996100d410601e0
José Alvarado <josealvarado2117@gmail.com>
2022-08-14 02:17:20 -0400

    frontend config y lista de productos

---
+++ README.md
@@ -0,0 +1,8 @@
+# MERN ECOMMERCE AMAZONA
+
+## Lessons
+
+1. Introducción
+2. Instkación de herramientas
+3. npm create vite@latest frontend --template react
+4. Crear repositorio de git
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import data from './data'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,31 @@ function App() {
         <a href="/">Amazona</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <article className="products">
+          {
+            data.products.map(product => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={`${product.image}`} alt={`${product.name}`}/>
+                </a>
+                <div className="product__body">
+                  <p className="product__name">{product.name}</p>
+                  <p className="product__reviews">{product.numReviews} reviews</p>
+                  <p className="product__price">
+                    <strong>${product.price}</strong>
+                  </p>
+                  <div className="product__card">
+                    <button 
+                      className="product__button"
+                    >Add to Card</button>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </article>
+        
       </main>
     </div>
   )
