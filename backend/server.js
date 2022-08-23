858fd0056a31d38c174902f7bf344e45b3c42b2b
José Alvarado <josealvarado2117@gmail.com>
2022-08-23 01:25:58 -0400

    agregando las rutas de productos y subiendo a mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -3,45 +3,18 @@ import cors from 'cors';
 
 import data from './data/data.js'
 import dbConnect from './config/database.js';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 const app = express();
 
 // conección a la base de datos
 dbConnect()
 
-
 app.use(cors())
 
-
-app.get('/api/products', (req, res) => {
-  res.json(data.products);
-})
-
-
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find(item => item.slug === req.params.slug);
-
-  if (product) {
-    res.json(product);
-  }
-  else {
-    res.status(404).json({ message: 'Producto no encontrado.'})
-  }
-})
-
-
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find(item => item._id === req.params.id);
-
-  if (product) {
-    res.json(product);
-  }
-  else {
-    res.status(404).json({ message: 'Producto no encontrado.'})
-  }
-})
-
-
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter)
 
 
 const port = process.env.PORT || 4005;
