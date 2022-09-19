41029c84b0544f566f3144d7632cbea5e63801c7
José Alvarado <josealvarado2117@gmail.com>
2022-09-19 03:23:08 -0400

    bugs

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter)
 
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
 app.get('*', (req, res) => 
   res.sendFile(path.join(__dirname, '/frontend/dist/index.html'))
 )
