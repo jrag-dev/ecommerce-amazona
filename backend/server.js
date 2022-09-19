e496b06f87d002ca18b407c56cc11d7bb29abe26
José Alvarado <josealvarado2117@gmail.com>
2022-09-19 02:27:05 -0400

    desply en heroku

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 
 import dbConnect from './config/database.js';
 import seedRouter from './routes/seedRoutes.js';
@@ -25,6 +26,12 @@ app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter)
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+)
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message })
